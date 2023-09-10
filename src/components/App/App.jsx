641d5296007a7d944293e87683ef6cc7f77cf115
BogdanK85 <kobakbogdan@gmail.com>
2023-09-10 18:59:03 +0300

    add notification + validation

---
+++ src/components/App/App.jsx
@@ -0,0 +1,89 @@
+import { Filter } from 'components/Filter/Filter';
+import { Notification } from 'components/Notification/Notification';
+import { nanoid } from 'nanoid';
+import { Component } from 'react';
+import { ContactForm } from '../ContactForm/ContactForm';
+import { ContactList } from '../ContactList/contactList';
+import { MainTitle, SecondTitle } from './App.styled';
+
+export class App extends Component {
+  state = {
+    contacts: [
+      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+    ],
+    filter: '',
+    name: '',
+    number: '',
+  };
+  addContact = dataContact => {
+    const haveContactAlready = this.state.contacts.find(
+      contact => contact.name === dataContact.name
+    );
+
+    if (haveContactAlready) {
+      return alert(`${dataContact.name} is already in contacts`);
+    }
+    const newContact = {
+      ...dataContact,
+      id: nanoid(),
+    };
+
+    this.setState(prevState => ({
+      contacts: [newContact, ...prevState.contacts],
+    }));
+  };
+
+  inputFilterShift = ({ target: { value } }) => {
+    this.setState({
+      filter: value,
+    });
+  };
+
+  getFilteredContacts = () => {
+    const { contacts, filter } = this.state;
+    return contacts.filter(contact =>
+      contact.name.toLowerCase().includes(filter.toLowerCase())
+    );
+  };
+
+  deleteContact = contactId => {
+    this.setState(prevState => ({
+      contacts: prevState.contacts.filter(contact => contact.id !== contactId),
+    }));
+  };
+
+  checkContactExist = () => {
+    return this.state.contacts.length;
+  };
+
+  render() {
+    // const { contacts } = this.state;
+    const filteredContacts = this.getFilteredContacts();
+    return (
+      <div>
+        <MainTitle>Phonebook</MainTitle>
+        <ContactForm addContact={this.addContact} />
+        <SecondTitle>Contacts</SecondTitle>
+        <Filter
+          inputFilterShift={this.inputFilterShift}
+          filter={this.state.filter}
+        />
+        <div>
+          {this.checkContactExist() === 0}
+          {this.getFilteredContacts().length > 0 ? (
+            <ContactList
+              contacts={filteredContacts}
+              filter={this.state.filter}
+              onDeleteContact={this.deleteContact}
+            />
+          ) : (
+            <Notification message="There are no contacts in this list" />
+          )}
+        </div>
+      </div>
+    );
+  }
+}
--- src/components/Filter/Filter.jsx
+++ src/components/Filter/Filter.jsx
@@ -1,17 +1,18 @@
-import { Input } from "components/ContactForm/ContactForm.styled"
+// // import { Input } from 'components/ContactForm/ContactForm.styled';
+// import { FilterStyle, FilterText, Input } from './filter.styled';
 
-export const Filter = ({ inputFilter, filter }) => {
-    return (
-        <label>
-            <p>Find contact by name</p>
-            <Input
-                name="filter"
-                value={filter}
-                type="text"
-                placeholder="Search contacs"
-                pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
-                onChange={inputFilter}
-            />
-        </label>
-    )
-}
+// export const Filter = ({ inputFilterShift, filter }) => {
+//   return (
+//     <FilterStyle>
+//       <FilterText>Find contact by name</FilterText>
+//       <Input
+//         name="filter"
+//         value={filter}
+//         type="text"
+//         placeholder="Search contacs"
+//         pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+//         onChange={inputFilterShift}
+//       />
+//     </FilterStyle>
+//   );
+// };
