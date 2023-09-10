56ef16d72a9cfcb8c0400161acedf4de2a8d6d15
BogdanK85 <kobakbogdan@gmail.com>
2023-09-10 19:00:18 +0300

    add style

--- src/components/Filter/Filter.jsx
+++ src/components/Filter/Filter.jsx
@@ -1,18 +1,18 @@
-// // import { Input } from 'components/ContactForm/ContactForm.styled';
-// import { FilterStyle, FilterText, Input } from './filter.styled';
+// import { Input } from 'components/ContactForm/ContactForm.styled';
+import { FilterStyle, FilterText, Input } from './filter.styled';
 
-// export const Filter = ({ inputFilterShift, filter }) => {
-//   return (
-//     <FilterStyle>
-//       <FilterText>Find contact by name</FilterText>
-//       <Input
-//         name="filter"
-//         value={filter}
-//         type="text"
-//         placeholder="Search contacs"
-//         pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
-//         onChange={inputFilterShift}
-//       />
-//     </FilterStyle>
-//   );
-// };
+export const Filter = ({ inputFilterShift, filter }) => {
+  return (
+    <FilterStyle>
+      <FilterText>Find contact by name</FilterText>
+      <Input
+        name="filter"
+        value={filter}
+        type="text"
+        placeholder="Search contacs"
+        pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+        onChange={inputFilterShift}
+      />
+    </FilterStyle>
+  );
+};
